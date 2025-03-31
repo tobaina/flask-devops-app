25b4fbb44084c4bd259a49fa0f9b0225ca6b5663
Ubuntu <ubuntu@ip-172-31-29-177.ca-central-1.compute.internal>
2025-03-31 03:34:32 +0000

    Add: Ansible deployment files and config

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def home():
     return render_template("index.html", messages=messages)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
