25edb53ed1dee88dcd4755692cb493fdd8269ad5
tobaina <tobaina@gmail.com>
2025-04-07 17:47:01 -0400

    edit table

--- init_db.sql
+++ init_db.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS messages (
     message TEXT NOT NULL
 );
 
-INSERT INTO messages (message) VALUES ("Welcome to your DevOps Flask app!");
+INSERT INTO messages (message) VALUES ("Welcome to your DevOps Flask app!<br>Project was done by Tobi & Believe</br>");
